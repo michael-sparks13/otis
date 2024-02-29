2b32b28cc4d38d03df69b7a6ac252060866f7ea0
Michael Sparks <115479949+michael-sparks13@users.noreply.github.com>
2024-02-29 13:18:19 -0500

    get slider map working

--- map.js
+++ map.js
@@ -13,57 +13,70 @@ const cones = "data/forecasts/cones.geojson";
 const landslides = "data/landslides.geojson";
 
 //CREATE SLIDER ELEMENT
-function createSliderElement() {
+function createSliderElement(data) {
   overlay = document.createElement("div");
   overlay.id = "map-overlay";
   overlay.innerHTML =
     '<h2 id="slider-title">5 Day Forecast on 10:00 AM Sun Oct 22</h2><label id="month"></label><input id="slider" type="range" min="0" max="23" step="1" value="0" />';
   document.getElementById("map").appendChild(overlay);
 
-  updateFcMap();
+  createFcMap("1");
+  updateFcMap(data[0], data[1]);
 }
 
-
 //INVOKE FUNCTIONS
-//createFcMap("1");
-// createFcMap("2");
-// createFcMap("5");
-// createFcMap("9A");
+// createFcMap("1");
+//createFcMap("2");
+createFcMap("5");
+createFcMap("9A");
 //createLandslides();
-createSliderElement();
-//updateFcMap();
+//createSliderMap();
 
-
-
-
-
+function createSliderMap() {
+  Promise.all([fetch(lines), fetch(cones)])
+    .then((responses) => {
+      return Promise.all(
+        responses.map((response) => {
+          return response.json();
+        })
+      );
+    })
+    .then((data) => {
+      console.log(data); // An array of results.
+      createSliderElement(data);
+      
+    })
+    .catch((error) => {
+      console.error("Something went wrong:", error);
+    });
+}
 
 //UPDATE FC MAP BASED ON SLIDER INPUT
-function updateFcMap() {
+function updateFcMap(lines, cones) {
   document.getElementById("slider").addEventListener("input", (e) => {
     let advisCount = e.target.value;
-    console.log('n');
+    console.log("n");
 
-  //   let advisNum = cones["features"][advisCount]["properties"]["ADVISNUM"];
+      let advisNum = cones["features"][advisCount]["properties"]["ADVISNUM"];
 
-  //   let advisDate = cones["features"][advisCount]["properties"]["ADVDATE"];
+      let advisDate = cones["features"][advisCount]["properties"]["ADVDATE"];
 
-  //   console.log("advisDate", advisDate);
+      console.log("advisDate", advisDate);
 
-  //   advisDate = advisDate.replace("2023", "");
-  //   advisDate = advisDate.replace("CDT", "");
+      advisDate = advisDate.replace("2023", "");
+      advisDate = advisDate.replace("CDT", "");
 
-  //   let colon = advisDate.indexOf("00");
-  //   //fix this because it grabs "10" unintentionally
-  //   advisDate = advisDate.slice(0, colon) + ":" + advisDate.slice(colon);
+      let colon = advisDate.indexOf("00");
+      //fix this because it grabs "10" unintentionally
+      advisDate = advisDate.slice(0, colon) + ":" + advisDate.slice(colon);
 
-  //   map.setFilter("cones", ["==", "ADVISNUM", advisNum]);
-  //   map.setFilter("lines", ["==", "ADVISNUM", advisNum]);
+      map.setFilter("cones", ["==", "ADVISNUM", advisNum]);
+      map.setFilter("lines", ["==", "ADVISNUM", advisNum]);
 
-  //   document.getElementById(
-  //     "slider-title"
-  //   ).innerText = `5 Day Forecast on ${advisDate}`;
-   });
+      document.getElementById(
+        "slider-title"
+      ).innerText = `5 Day Forecast on ${advisDate}`;
+  });
 }
 
 // CREATE CONES OF UNCERTAINTY
@@ -118,10 +131,8 @@ function createFcMap(advisNum) {
   createLines(advisNum);
 }
 
-
 //update FC map based on slider input
 function updateMap() {}
-
 
 //CREATE LANDSLIDES LAYER
 function createLandslides() {
@@ -142,14 +153,11 @@ function createLandslides() {
   });
 }
 
-
-
 //   <!-- //first is 18-e; 2nd is 6hrs later, TS Otis; advis 5 = 24 hrs after first, straight for AC
 //   //9A = first as hurricane. 1pm CDT on Tuesday
 //   //there were 13 advis btwn first overall and first as hurricane, 10am Sun to 1pm Tues (51 hours)
 //   //is this an unusual number? compare to others from that season. maybe hilary in CA -->
 //   //when did Otis become a numbered storm?
-
 
 //ADD SEA SURFACE TEMP ANOMALY LAYER
 // map.on("load", function () {

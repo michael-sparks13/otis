12c283081c07e42718a14ac7428fbe0f02cd91cf
Michael Sparks <115479949+michael-sparks13@users.noreply.github.com>
2024-03-07 10:56:04 -0500

    update fc map by step fxn added

--- scroll.js
+++ scroll.js
@@ -1,46 +1,46 @@
 
-// // Wait until the map is loaded to add the data
-// map.on("load", function () {
-//   // add the cones data source
-//   map.addSource("cones", {
-//     type: "geojson",
-//     data: "data/forecasts/cones.geojson",
-//   });
+// Wait until the map is loaded to add the data
+map.on("load", function () {
+  // add the cones data source
+  map.addSource("cones", {
+    type: "geojson",
+    data: "data/forecasts/cones.geojson",
+  });
 
-//   // add the cones layer to map
-//   map.addLayer({
-//     id: "cones",
-//     type: "fill",
-//     source: "cones",
-//     paint: {
-//       "fill-color": "#FF0000",
-//       "fill-opacity": 0.7,
-//     },
-//     filter: ["==", "ADVISNUM", "1"],
-//   });
+  // add the cones layer to map
+  map.addLayer({
+    id: "cones",
+    type: "fill",
+    source: "cones",
+    paint: {
+      "fill-color": "#FF0000",
+      "fill-opacity": 0.7,
+    },
+    filter: ["==", "ADVISNUM", "1"],
+  });
 
-//   // add the lines data source
-//   map.addSource("lines", {
-//     type: "geojson",
-//     data: "data/forecasts/lines.geojson",
-//   });
+  // add the lines data source
+  map.addSource("lines", {
+    type: "geojson",
+    data: "data/forecasts/lines.geojson",
+  });
 
-//   // add the lines layer to map
-//   map.addLayer({
-//     id: "lines",
-//     type: "line",
-//     source: "lines",
-//     layout: {
-//       "line-join": "round",
-//       "line-cap": "round",
-//     },
-//     paint: {
-//       "line-color": "#888",
-//       "line-width": 8,
-//     },
-//     filter: ["==", "ADVISNUM", "1"],
-//   });
-// });
+  // add the lines layer to map
+  map.addLayer({
+    id: "lines",
+    type: "line",
+    source: "lines",
+    layout: {
+      "line-join": "round",
+      "line-cap": "round",
+    },
+    paint: {
+      "line-color": "#888",
+      "line-width": 8,
+    },
+    filter: ["==", "ADVISNUM", "1"],
+  });
+});
 
 
 
@@ -48,22 +48,20 @@
 const updateMapData = (step) => {
   // Logic to update map based on the step
   if (step === 1) {
-    createFcMap("1");
-    console.log("Step 1", step);
+    map.setFilter("cones", ["==", "ADVISNUM", "1"]);
+    map.setFilter("lines", ["==", "ADVISNUM", "1"]);
     return;
   } else if (step === 2) {
-    createFcMap("2");
-    
-    console.log("Step 2", step);
+    map.setFilter("cones", ["==", "ADVISNUM", "2"]);
+    map.setFilter("lines", ["==", "ADVISNUM", "2"]);
     return;
   } else if (step === 3) {
-    createFcMap("5");
-    
-    console.log("Step 3", step);
+    map.setFilter("cones", ["==", "ADVISNUM", "5"]);
+    map.setFilter("lines", ["==", "ADVISNUM", "5"]);
     return;
   } else if (step === 4) {
-    createFcMap("9A");
-    console.log("Step 4", step);
+    map.setFilter("cones", ["==", "ADVISNUM", "9A"]);
+    map.setFilter("lines", ["==", "ADVISNUM", "9A"]);
     return;
     
   }
